7a9e8e87a8c4b99adc568795612beea39d23353e
Sebastian Rühmann <sebastian.ruehmann@pushh.it>
2016-12-04 13:22:48 +0100

    ✨ Added vue-ressource

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import Create from './Create.vue'
 import Dashboard from './Dashboard.vue'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const routes = [
   { path: '/dashboard', component: Dashboard },

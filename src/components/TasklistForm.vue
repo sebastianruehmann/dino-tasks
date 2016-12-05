c635f7a8217ddf5f75020e8b31581cad07e1a4fc
Sebastian Rühmann <sebastian.ruehmann@pushh.it>
2016-12-05 15:14:16 +0100

    ✨ Get API data for Detail-View of TasklistForm

--- src/components/TasklistForm.vue
+++ src/components/TasklistForm.vue
@@ -2,8 +2,8 @@
   <div class="create-form">
     <h1>Create</h1>
     <form>
-      <input type="text" v-model="title">
-      <task v-for="taskItemData in taskItemsData" v-on:newTaskItem="addNewTaskItem" :subject="taskItemData.subject"></task>
+      <input type="text" :value="title" v-model="title">
+      <task v-for="task in tasks" v-on:newTaskItem="addNewTaskItem" :subject="task.subject"></task>
       <input @click="newTasklist" @click.prevent.submit type="submit" value="Merken">
     </form>
   </div>
@@ -19,20 +19,31 @@ export default {
   },
   data: function () {
     return {
-      taskItemsData: [
-        {
-          subject: 'Hello'
-        },
-        {
-          subject: 'Test'
-        },
-        {}
-      ]
+      title: '',
+      tasks: null
     }
   },
+  created: function () {
+    this.fetchData()
+  },
   methods: {
+    fetchData: function () {
+      this.resource = this.$resource(window.location.protocol + '//' + window.location.hostname + ':5000/api/v1/tasklist{/id}')
+
+      const self = this
+
+      this.resource.get({id: '58455deef2934c118bd9a4cf'}).then((response) => {
+        return response.json()
+      }, (response) => {
+        return response.text()
+      }).then(result => {
+        self.title = result.title
+        self.tasks = result.tasks
+        self.tasks.push({})
+      })
+    },
     addNewTaskItem: function () {
-      this.taskItemsData.push({})
+      this.tasks.push({})
     },
     newTasklist: function () {
       this.$http.post(window.location.protocol + '//' + window.location.hostname + ':5000/api/v1/tasklists', { title: 'Hello', description: 'Lorem Ipsum' }).then((response) => {

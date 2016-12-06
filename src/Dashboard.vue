89b2d85ea24d973ccc0da48d4b49918aa303cc85
Sebastian Rühmann <sebastian.ruehmann@pushh.it>
2016-12-06 14:10:40 +0100

    ⭐️ Dashboard view with tasklists

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -1,29 +1,52 @@
 <template>
   <div id="app">
     <h1>Dashboard</h1>
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <tasklist v-for="tasklist in tasklists" :tasklist="tasklist"></tasklist>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Tasklist from './components/Tasklist'
 
 export default {
   name: 'dashboard',
   components: {
-    Hello
+    Tasklist
+  },
+  data: function () {
+    return {
+      tasklists: null
+    }
+  },
+  created: function () {
+    this.fetchData()
+  },
+  updated: function (val) {
+    console.log(val)
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData: function () {
+      const self = this
+
+      this.$http.get(window.location.protocol + '//' + window.location.hostname + ':5000/api/v1/tasklists').then((result) => {
+        self.tasklists = result.body
+      }, (response) => {
+        return response.text()
+      })
+    }
   }
 }
 </script>
 
-<style>
-#app {
+<style lang="sass">
+#app
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  text-align: left;
 </style>
---
+++ src/components/Tasklist.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tasklist">
+    <h2 class="tasklist-title">{{ title }}</h2>
+    <p class="tasklist-description">{{ description }}</p>
+    <router-link class="tasklist-edit" :to="tasklistLink">Show</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tasklist',
+    props: ['tasklist'],
+    data: function () {
+      return {
+        id: undefined,
+        title: '',
+        description: '',
+        tasklistLink: {},
+        tasklist: {}
+      }
+    },
+    created: function () {
+      this.id = this.tasklist._id
+      this.title = this.tasklist.title
+      this.description = this.tasklist.description
+      this.tasklistLink = {
+        name: 'tasklist.edit',
+        params: {id: this.id}
+      }
+    }
+  }
+</script>
+<style lang="sass">
+  .tasklist
+    border-bottom: 1px solid #333;
+    padding: 0.3rem 0;
+    margin: 0.1rem 0;
+    position: relative;
+  .tasklist-title
+    margin: 0.1rem 0 0;
+  .tasklist-description
+    margin: 0;
+  .tasklist-edit
+    position: absolute;
+    bottom: 0.5rem;
+    right: 0;
+    background: #333;
+    color: white;
+    text-decoration: none;
+    padding: 0.3rem;
+</style>

83d7f16e910ead72d9e1ba3c091008d8f6e28fa9
Sebastian Rühmann <mail@sebastianruehmann.de>
2017-01-10 23:58:36 +0100

    ✨ GET Endpoint for all tasks

--- api/routes/tasks/get.js
+++ api/routes/tasks/get.js
@@ -1,5 +1,15 @@
 const Task = require('./../../models/task')
 
+module.exports.list = function (req, res) {
+  Tasklist.find({}, function (err, response) {
+    if (err) {
+      throw err;
+    }
+
+    res.json(response);
+  })
+}
+
 module.exports.one = function (req, res) {
   const taskId = req.params.id;
 

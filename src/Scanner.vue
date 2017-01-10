638ecf031ad3be1411e75c1c1a9f25edc1af1e31
Sebastian Rühmann <mail@sebastianruehmann.de>
2017-01-10 17:55:55 +0100

    ✨ implemented scanner and printer

--- src/Scanner.vue
+++ src/Scanner.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <button @click.prevent="scanner">Scanner</button>
+    <button id="qrcode" @click.prevent="scanner" v-if="cordova">Scanner</button>
+    <hr>
+    <label for="code">Code eingeben:</label>
+    <input type="text" id="code" v-model="code">
+    <button @click.prevent="handleCodeSubmitClick">˃</button>
   </div>
 </template>
 
@@ -10,11 +14,21 @@ import Scanner from './plugins/scanner'
 export default {
   data: function () {
     return {
+      cordova: window.cordova,
+      code: ''
     }
   },
   created: function () {
   },
   methods: {
+    handleCodeSubmitClick: function () {
+      if (this.checkID(this.code)) {
+        this.$router.push({name: 'tasklist.edit', params: { id: this.code }})
+      }
+    },
+    checkID: function (val) {
+      return val.match(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i)
+    },
     scanner: function () {
       const barcodeScanner = new Scanner().$
       if (!barcodeScanner) {
@@ -40,7 +54,7 @@ export default {
           prompt: 'Place a barcode inside the scan area', // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           formats: 'QR_CODE', // default: all but PDF_417 and RSS_EXPANDED
-          orientation: 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
+          orientation: 'potrait', // Android only (portrait|landscape), default unset so it rotates with the device
           disableAnimations: true // iOS
         }
       )
@@ -52,4 +66,7 @@ export default {
 <style lang='sass' scoped>
   @import 'scss/globals';
 
+  #qrcode
+    width: 100%;
+    height: 50%;
 </style>

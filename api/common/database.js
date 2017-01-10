f10ad8a1ca2417489109bb8ff361901554032fa6
Sebastian Rühmann <mail@sebastianruehmann.de>
2017-01-10 23:27:55 +0100

    🚑 throw error when database crashes

--- api/common/database.js
+++ api/common/database.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
-const settings = require('../settings.js')
+const settings = require('../settings')
+const log = require('./logging')
 
 module.exports = function () {
   var dbHost = 'mongodb://localhost/' + settings.database;
   mongoose.connect(dbHost);
+
+  mongoose.connection.on('error', function (err) {
+   log.error(err)
+  });
 }

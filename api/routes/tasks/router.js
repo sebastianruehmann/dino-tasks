dc49128d59726c28535c5e27c3832c4b0977ca3a
Sebastian Rühmann <mail@sebastianruehmann.de>
2017-01-10 23:58:46 +0100

    ✨ Activate routes

--- api/routes/tasks/router.js
+++ api/routes/tasks/router.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const get = require('./get');
 const post = require('./post');
-// const delete = require('./delete');
+const delete = require('./delete');
 const put = require('./put');
 
-// router.get('/', get.list)
-// router.get('/:id', get.one)
+router.get('/', get.list)
+router.get('/:id', get.one)
 
 router.post('/', post.one)
 
-// router.delete('/:id', delete.one)
+router.delete('/:id', delete.one)
 
 router.put('/:id', put.one)
 

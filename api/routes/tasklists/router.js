9d080397f7bcbd0ae7ab898c5a0345cade005c6a
Sebastian Rühmann <mail@sebastianruehmann.de>
2017-01-11 00:54:55 +0100

    🐛 used remove instead of system variable delete

--- api/routes/tasklists/router.js
+++ api/routes/tasklists/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const get = require('./get');
 const post = require('./post');
-// const delete = require('./delete');
+const remove = require('./delete');
 const put = require('./put');
 
 router.get('/', get.list)
@@ -11,7 +11,7 @@ router.get('/:id', get.one)
 
 router.post('/', post.one)
 
-// router.delete('/:id', delete.one)
+router.delete('/:id', remove.one)
 
 router.put('/:id', put.one)
 
